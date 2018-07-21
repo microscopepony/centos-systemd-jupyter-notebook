384b6f014e24d1573df80d918ba0b40a55fe5344
Simon Li <orpheus+devel@gmail.com>
2018-07-21 12:58:00 +0100

    start systemd if first arg, otherwise start-notebook.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,14 @@
 set -ex
 
 echo Starting container with arguments "$@"
-# Escaping quotes https://unix.stackexchange.com/a/422170
-printf "NOTEBOOK_ARGS='%s'\n" "$(echo $@ | sed "s/'/'\"'\"'/g")" > /etc/jupyter/jupyter-notebook-service-env
-systemctl environment jupyter-notebook
-exec /usr/bin/systemd -vvvv init
+
+if [ "$1" == "systemd" ]; then
+  shift
+  # Escaping quotes https://unix.stackexchange.com/a/422170
+  printf "NOTEBOOK_ARGS='%s'\n" "$(echo $@ | sed "s/'/'\"'\"'/g")" > /etc/jupyter/jupyter-notebook-service-env
+ systemctl environment jupyter-notebook
+  exec /usr/bin/systemd -vvvv init
+else
+  # JupyterHub spawners may pass strangely quoted arguments so use $*
+  exec tini -g -- su jovyan -c "exec /usr/local/bin/start-notebook.sh $*"
+fi
